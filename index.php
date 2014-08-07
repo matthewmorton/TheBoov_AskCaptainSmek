94b19369338163be16347cb03e3554d4f188ca4b
Matthew Morton <matthewmorton100@gmail.com>
2014-08-07 13:08:09 -0400

    adding source code

---
+++ index.php
@@ -0,0 +1,205 @@
+<?php include("/home/content/16/8519016/html/PhotoBoov/password_protect.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Ask the Boov</title>
+<!--
+Author: Matthew Morton		
+email: matthewmorton100@gmail.com
+--> 	
+        <!--scripts-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<!--styles-->
+        <link type='text/css' rel='stylesheet' href='css/bootstrap.css' />
+        <link type='text/css' rel='stylesheet' href='css/bootstrap-responsive.min.css' />
+        <link type='text/css' rel='stylesheet' href='css/main.css' />
+        <!--google fonts-->
+		<link href='http://fonts.googleapis.com/css?family=Exo:400,700,900' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>    
+<script>
+$( document ).ready( function() {
+    $( '#myModal' ).modal( 'toggle' );
+});
+</script>
+
+</head>
+<body>
+
+<div id="myModal" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4><strong>Ogilvy Presents...</strong></h4>
+        <br>
+        <h1 class="modal-title">"ASK THE BOOV"</strong></h1>
+      </div>
+      <div class="modal-body">
+      	<h4><strong>Objective</strong></h4>
+        <br /><br />
+        <p>Help DreamWorks generate buzz and get fans engaged leading up to the release of their new animated feature "Home," staring Jim Parsons, Rihanna, Jennifer Lopez, and Steve Martin.</p>
+        <p>What if humans had the ability to talk to Aliens? What could possibly be more interesting than that? Man-kind has forever asked questions of the Universe; is there intelligent life deep out in space? Well, maybe not intelligent, we'll let you decide &ndash; did you <a href="http://youtu.be/GJY6fpoWYRw">watch the trailer</a>?! Anyway, let's ask the Boov and finally get some answers.</p>
+        <br />
+        <h4><strong>Solution</strong></h4>
+        <br /><br />
+        <p>Before the movie hits the theaters, let's let fans engage directly with Captain Smek, leader of the Boov. Through live Q & A sessions, for the first time in history DreamWorks can create an original transmission of social content from humans to Aliens and get fans excited for the movie’s release. Instantly give insight to the Boov and allow fans to unlock questions about the Universe. Leveraging the power of Twitter, humans can finally transmit questions deep into space and tweet with Aliens. Participants can use the #AskTheBoov hashtag to converse directly with Captain Smek of the Boov, amplifying the conversation and spreading the buzz even further.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+
+<div id="header-wrapper">
+	<div class="column-left">
+    	<img src="images/captain_smek.png" style="vertical-align:baseline;" class="float-right" alt="Captain Smek" width="250" height="250" />
+    </div>
+    
+    <div class="column-right">
+    
+    	<div id="topCircles">
+			<div class="wheels1" style="display: block; background-position: -1200px 0px;"></div>
+		</div>
+    
+    	<h1 class="float-left">Ask<br>Captain<br>Smek</h1>
+    	<h4>What if you could transmit into outer space and talk directly with Aliens? DreamWorks made it happen. Tweet your questions @GiammaNow using <span style="color:#0FF">#AskTheBoov</span> and get insider information and exclusive commentary from the Boov.</h4><h4><span style="color:#0FF">We'll do our best to translate.</span><a href="https://twitter.com/intent/tweet?url=www.meettheboov.com&via=GiammaNow&text=I%20have%20a%20question...&hashtags=AskTheBoov"><img src="images/tweetbutton.png" alt="tweet" width="185" height="40" style="margin-left:10px;" /></a></h4>
+        <h4>Invading Theaters November 26th</h4>
+        
+    
+    </div><!-- end column-right--> 
+    <div class="clearfix"></div>
+
+<div id="social"></div>
+
+</div><!-- header-wrapper-->
+
+
+
+<div id="main-container">
+	
+    <div class="tweet-box no-glow">
+    	<h3>Newest Questions...</h3>
+    </div>
+    
+	<div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/tweetPerson1.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@MortyNorts<br /><span style="color:#0FF">@GiammaNow</span> What kind of technology will the Boov bring to Earth? Do the Boov use iPads? <span style="color:#0FF">#AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />The Boov use iPads, we love Angry Birds. Wait until you see our BoovPads.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/tweetPerson2.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@DavidReily<br /><span style="color:#0FF">@GiammaNow</span> How do your propulsion systems work? Are the Boov immortal?<span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/tweetPerson3.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@JohnSmith<br /><span style="color:#0FF">@GiammaNow</span>What are your intentions with earth and humans? Can I hang out with you for a while, maybe see your planet?<span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/personTweetGirl1.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@JessicaPahlor<br /><span style="color:#0FF">@GiammaNow</span> How does your spaceship work? Can I Drive That Thing?<span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/personTweetGirl2.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@AshleyHubiak<br /><span style="color:#0FF">@GiammaNow</span> What Was The Blood Alcohol Content Of The Aliens That Crashed In Roswell? <span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/tweetPerson4.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@TaylorMyers<br /><span style="color:#0FF">@GiammaNow</span> Which One Of You Guys Played In E.T.? Do you have a favorite Earth movie? <span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+    
+    <div class="tweet-box">
+    	<div class="question-box">
+        	<div class="tweet-author"><img src="images/tweetPerson5.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@JeffreySudiam<br /><span style="color:#0FF">@GiammaNow</span> How Many Versions Of Team Rocket From Pokémon Have You Guys Found Floating In Space? <span style="color:#0FF"> #AskTheBoov</span><br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="answer-box">
+        	<div class="tweet-author"><img src="images/tweetAuthorSmek.png" width="60" height="60" style="border:1px solid #fff;" /></div>
+        	<div class="tweet-text">@GiammaNow<br />Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas.<br><br><img src="images/retweet.png" width="87" height="18" /></div>
+        </div>
+        <div class="clearfix"></div>
+    </div><!-- end tweet-box-->
+	
+    <div id="mast-head">
+    <h1><span style="color:#0FF;">#AskTheBoov</span></h1>
+    
+    <div id="theboov"><img src="images/theboov.png" alt="theboov" width="754" height="245" /></div>
+        
+    </div><!-- end mast-head-->
+
+    
+</div><!-- main-content-->
+
+<div id="bottomCircles">
+	<div class="wheels2"></div>
+</div>
+
+<div id="pageBackground"></div>
+
+</body>
+</html>
